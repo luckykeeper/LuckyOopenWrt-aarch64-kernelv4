951018457c04f826298fcba95db9131fb69dc3d2
Luckykeeper <34579433+luckykeeper@users.noreply.github.com>
2021-04-16 10:58:59 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,13 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-git clone https://github.com/crotoc/luci-app-v2ray.git package/luci-app-v2ray
-git clone https://github.com/2512500960/zzu-minieap-openwrt.git package/mini-eap
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/ysc3839/openwrt-minieap.git package/minieap
+git clone https://github.com/ysc3839/luci-proto-minieap.git package/luci-proto-minieap
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+git clone https://github.com/toss-a/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/kevin-morgan/luci-theme-argon-dark.git package/luci-theme-argon-dark
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/stustanet/luci-theme-ssn.git package/luci-theme-ssn
